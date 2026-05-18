c27cce95ae727e0304014e97b362c42c96c71e68
CppCXY <812125110@qq.com>
2026-05-18 17:27:20 +0800

    add debug lua_tests

--- run_lua_tests.ps1
+++ run_lua_tests.ps1
@@ -1,2 +1,47 @@
-cd lua_tests/testes; ../../target/release/lua.exe all.lua
-cd ../..
+param(
+	[ValidateSet("debug", "release")]
+	[string]$Profile = "debug",
+
+	[string]$Script = "all.lua",
+
+	[switch]$SkipBuild
+)
+
+$ErrorActionPreference = "Stop"
+
+if (-not $SkipBuild) {
+	if ($Profile -eq "debug") {
+		cargo build -p luars_interpreter
+	}
+	else {
+		cargo build -p luars_interpreter --release
+	}
+}
+
+if ($Profile -eq "debug") {
+	if (-not $env:LUARS_MAIN_STACK_SIZE_MB) {
+		$env:LUARS_MAIN_STACK_SIZE_MB = "128"
+	}
+	if (-not $env:LUARS_MAX_CALL_DEPTH) {
+		$env:LUARS_MAX_CALL_DEPTH = "1024"
+	}
+	if (-not $env:LUARS_MAX_C_STACK_DEPTH) {
+		$env:LUARS_MAX_C_STACK_DEPTH = "200"
+	}
+}
+
+$exe = if ($Profile -eq "debug") {
+	"../../target/debug/lua.exe"
+}
+else {
+	"../../target/release/lua.exe"
+}
+
+Push-Location "lua_tests/testes"
+try {
+	& $exe $Script
+	exit $LASTEXITCODE
+}
+finally {
+	Pop-Location
+}
